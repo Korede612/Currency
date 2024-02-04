f7c8349234d90c2bf469a687c1d51d0f49b3b6c2
Korede612 <korede.oko-osi@nytengaletech.com>
2024-02-04 08:56:52 +0100

    fix: Renamed Default ViewController

--- CurrencyApp/CurrencyViewController.swift
+++ CurrencyApp/CurrencyViewController.swift
@@ -7,14 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CurrencyViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = .systemPurple
     }
-
 
 }
 
